9116788b6bbc7e5031609233508af813d00e859e
Ganlv <ganlvtech@qq.com>
2019-03-22 03:16:36 +0800

    Add head img API. Change API name

--- kahla/api.go
+++ kahla/api.go
@@ -2,6 +2,8 @@ package kahla
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/url"
 	"time"
 )
@@ -70,24 +72,24 @@ func (s *AuthService) InitPusher() (*InitPusherResponse, error) {
 	return response, nil
 }
 
-type LoadFriendsResponse struct {
+type MyFriendsResponse struct {
 	Items []struct {
-		DisplayName       string      `json:"displayName"`
-		DisplayImageKey   int         `json:"displayImageKey"`
-		LatestMessage     string      `json:"latestMessage"`
-		LatestMessageTime time.Time   `json:"latestMessageTime"`
-		UnReadAmount      int         `json:"unReadAmount"`
-		ConversationID    int         `json:"conversationId"`
-		Discriminator     string      `json:"discriminator"`
-		UserID            interface{} `json:"userId"`
-		AesKey            string      `json:"aesKey"`
-		Muted             bool        `json:"muted"`
+		DisplayName       string    `json:"displayName"`
+		DisplayImageKey   int       `json:"displayImageKey"`
+		LatestMessage     string    `json:"latestMessage"`
+		LatestMessageTime time.Time `json:"latestMessageTime"`
+		UnReadAmount      int       `json:"unReadAmount"`
+		ConversationID    int       `json:"conversationId"`
+		Discriminator     string    `json:"discriminator"`
+		UserID            string    `json:"userId"`
+		AesKey            string    `json:"aesKey"`
+		Muted             bool      `json:"muted"`
 	} `json:"items"`
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
-func (s *FriendshipService) LoadFriends() (*LoadFriendsResponse, error) {
+func (s *FriendshipService) MyFriends() (*MyFriendsResponse, error) {
 	resp, err := s.client.client.Get(KahlaServer + "/friendship/MyFriends?orderByName=false")
 	if err != nil {
 		return nil, err
@@ -100,7 +102,7 @@ func (s *FriendshipService) LoadFriends() (*LoadFriendsResponse, error) {
 		return nil, &ResponseStatusCodeNot200{Response: resp, StatusCode: resp.StatusCode}
 	}
 	j := json.NewDecoder(resp.Body)
-	response := &LoadFriendsResponse{Code: -1}
+	response := &MyFriendsResponse{Code: -1}
 	err = j.Decode(response)
 	if err != nil {
 		return response, &ResponseJsonDecodeError{response.Message, err}
@@ -109,4 +111,16 @@ func (s *FriendshipService) LoadFriends() (*LoadFriendsResponse, error) {
 		return response, &ResponseCodeNotZero{response.Message}
 	}
 	return response, nil
+}
+
+func (s *FriendshipService) HeadImgFile(headImgFileKey int) ([]byte, error) {
+	resp, err := s.client.client.Get(fmt.Sprintf("https://oss.aiursoft.com/download/fromkey/%d?w=100&h=100", headImgFileKey))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, &ResponseStatusCodeNot200{Response: resp, StatusCode: resp.StatusCode}
+	}
+	return ioutil.ReadAll(resp.Body)
 }

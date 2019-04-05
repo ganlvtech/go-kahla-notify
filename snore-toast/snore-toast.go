006b3d4a91df6df5427c378c87fe47d7fd64d956
Ganlv <ganlvtech@qq.com>
2019-04-05 18:25:45 +0800

    refactor toast

---
+++ snore-toast/snore-toast.go
@@ -0,0 +1,32 @@
+package snore_toast
+
+import (
+	"os/exec"
+)
+
+type SnoreToast struct {
+	Path string
+}
+
+func New(path string) *SnoreToast {
+	if path == "" {
+		path = "SnoreToast.exe"
+	}
+	return &SnoreToast{Path: path}
+}
+
+func (s *SnoreToast) Toast(title string, message string) error {
+	err := exec.Command(s.Path, "-t", title, "-m", message, "-s", "Notification.IM").Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SnoreToast) ToastWithImage(title string, message string, imagePath string) error {
+	err := exec.Command(s.Path, "-t", title, "-m", message, "-p", imagePath, "-s", "Notification.IM").Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
